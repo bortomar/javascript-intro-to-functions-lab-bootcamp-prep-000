8769a43acd11b1f88dbe00471bb52cafe4e41043
Mario Bortolotti <bortomar@email.cz>
2018-05-22 17:36:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 function shout(p) {
   return p.toUpperCase();
 }
+
 function whisper(p) {
   return p.toLowerCase();
 }
 
 function logShout(p) {
   console.log(shout(p));
+}
+
+function logWhisper(p) {
+  console.log(whisper(p));
 }
